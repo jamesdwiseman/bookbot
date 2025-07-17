eaa7f36b88810505c9bb1eb25fdf073f6a091778
james.wiseman <james.daniel.wiseman@proton.me>
2025-07-17 11:20:34 -0500

    Added count_words function, incorporated into main function

--- main.py
+++ main.py
@@ -4,5 +4,10 @@ def get_book_text(path):
     return file_contents
 def main():
     book_variable = get_book_text("books/frankenstein.txt")
-    print(book_variable)
+    count_words(book_variable)
+
+def count_words(book_text):
+    text = book_text
+    num_words = len(text.split())
+    print(f"{num_words} words found in the document")
 main()
