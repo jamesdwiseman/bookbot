047f76c1b929e01e04505a6745a3744ef4ce11c3
james.wiseman <james.daniel.wiseman@proton.me>
2025-07-21 15:26:18 -0500

    Added mobydick.txt and prideandprejudice.txt, altered main.py to accept sys.argv

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import (
     count_words,
     count_characters,
@@ -8,11 +9,15 @@ def get_book_text(path):
         file_contents = f.read()
     return file_contents
 def main():
-    book_variable = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2: #checks if the user provided the wrong number of arguments
+     #sys.argv is a list of command line arguments; we want exactly 2 (script name and book path)
+        print(f"Usage: python3 main.py <path_to_book>") #if the user did not provide right arguments, show them how to use the program correclty
+        sys.exit(1) #stop the program immediately; 1 is an exit code that tells the system the program failed
+    book_variable = get_book_text(sys.argv[1])
     result1 = count_words(book_variable) # word count
     result2 = count_characters(book_variable) # character dictionary
     print(f"""============ BOOKBOT ============
-    Analyzing book found at books/frankenstein.txt...
+    Analyzing book found at {sys.argv[1]}...
     ----------- Word Count ----------
     Found {result1} total words
     --------- Character Count ------""")
